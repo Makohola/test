b766c5c8b319541e02357aedfe07d68ee4fd1d3e
Makohola <100530013+Makohola@users.noreply.github.com>
2025-02-23 18:21:49 +1000

    Update server.php

--- server.php
+++ server.php
@@ -3,7 +3,7 @@
 use Workerman\Worker;
 require_once __DIR__ . '/vendor/autoload.php';
 
-$wsWorker = new Worker('websocket://31.128.42.42:2346');
+$wsWorker = new Worker('websocket://0.0.0.0:2346');
 $wsWorker ->count = 4;
 
 $wsWorker->onConnect = function ($connection) {
